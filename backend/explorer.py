7ce8dfa02a9583d30860e21648c28f3841932c52
Peter Griggs <pgriggs@mit.edu>
2020-07-21 23:05:25 -0700

    perf debugging, added spacy pipeline for clean, add gunicorn for serving requests using >1 proc

--- backend/explorer.py
+++ backend/explorer.py
@@ -9,5 +9,5 @@ app = create_app()
 migrate = Migrate(app, db)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, threaded=True, processes=4)
--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 rm *.pkl
 flask db upgrade
-flask run --host=0.0.0.0 --port=5000
+# flask run --host=0.0.0.0 --port=5000
+exec gunicorn -w 9 --threads 12 -b :5000 explorer:app --preload
 
